01a318ee175787f4e162b55cc96196b7fb619c62
Nguyễn Hữu Tài <vnnguyenhuutai2000@gmail.com>
2022-07-20 06:02:00 +0700

    feat: update code

--- test3.js
+++ test3.js
@@ -5,6 +5,7 @@ const url = "https://finance.vietstock.vn/image/AAT"
 
 async function download(url,filename) {
   const response = await fetch(url);
+  // console.log(response)
   const buffer = await response.buffer();
   fs.writeFile(`./images/${filename}`, buffer, () => 
     console.log('finished downloading' + filename));
@@ -14,6 +15,10 @@ let rawdata = fs.readFileSync('data-stock.json');
 let data = JSON.parse(rawdata);
 console.log(data.length)
 data.map((item, index) => {
-    if(index >= 1452 && index < 1453)
-        download(`https://finance.vietstock.vn/image/${item.machungkhoan}`,`${item.machungkhoan}.jpeg`)
+    // if(index >= 1452 && index < 1453)
+    setTimeout(
+      () => download(`https://finance.vietstock.vn/image/${item.machungkhoan}`,`${item.machungkhoan}.jpeg`)
+      ,500 * index
+    )
 });
+// download(`https://finance.vietstock.vn/image/VNB`,`VNB.jpeg`)
